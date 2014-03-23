429d9ca4b6301996bede8f3097e5bf1933fd7ff2
Egor Artemov <egor.artemov@gmail.com>
2014-03-23 05:15:03 +0400

    Remove GLX_X_VISUALTYPE from FbConfig attributes
    This attribute is ignored in GLX1.4 and not striclty required anyway

--- X11/main_x11.c
+++ X11/main_x11.c
@@ -184,7 +184,6 @@ static int modern_run (Display *display, int screen)
         GLX_X_RENDERABLE, True,
         GLX_DRAWABLE_TYPE, GLX_WINDOW_BIT,
         GLX_RENDER_TYPE, GLX_RGBA_BIT,
-        GLX_X_VISUAL_TYPE, GLX_TRUE_COLOR,
         GLX_RED_SIZE, 8,
         GLX_GREEN_SIZE, 8,
         GLX_BLUE_SIZE, 8,
