ebbf25ec2a61c9dcdbbc61edb0611b79b9170e1a
Egor Artemov <egor.artemov@gmail.com>
2014-03-24 06:30:57 +0400

    Encapsulate glx interface

--- X11/main_x11.c
+++ X11/main_x11.c
@@ -8,15 +8,32 @@
 #define PACKAGE_STRING "glbootstrap 0.1"
 #define PACKAGE_BUGREPORT "egor.artemov@gmail.com"
 
-typedef struct glx_context_info_t {
-    /* common glx data */
-    GLXContext context;
-    XVisualInfo *visual_info;
+typedef struct UGL {
+    PFNGLXCREATECONTEXTATTRIBSARBPROC glXCreateContextAttribsARB;
+    Display *display;
+    int screen;
+    /** Major version of glx */
+    int glx_major;
+    /** Minor version of glx */
+    int glx_minor;
+    /** Flag that indicates that we have legacy GLX version (GLX <=1.2) */
+    int is_legacy;
+    /** Flag that indicates that GLX_ARB_create_context_profile is implemented*/
+    int is_arb_context_profile;
+    char padding[4];
+} UGL;
 
-    /* modern glx data */
-    GLXFBConfig best_fbconfig;
-    GLXWindow glx_window;
-} glx_context_info_t;
+typedef void UGLFrameBufferConfig;
+typedef Window UGLNativeWindow;
+
+typedef struct UGLRenderSurface {
+    GLXContext context;
+    GLXDrawable drawable;
+} UGLRenderSurface;
+
+enum UGLConfigAttribute {
+    UGL_NATIVE_VISUAL_ID
+};
 
 /** Window type */
 typedef struct game_window_t {
@@ -31,20 +48,6 @@ typedef struct game_window_t {
 } game_window_t;
 
 static game_window_t *main_window = NULL;
-
-static glx_context_info_t context_info = {NULL, NULL, NULL, (GLXWindow)NULL};
-
-/** Version of glx */
-static int glx_major, glx_minor;
-/** Flag that indicates that we have legacy GLX version (GLX <=1.2) */
-static int glx_legacy = 0;
-
-/* GLX_ARB_create_context */
-static int arb_create_context_extension = 0;
-
-/* GLX_ARB_create_context_profile */
-static int arb_context_profile = 0;
-static PFNGLXCREATECONTEXTATTRIBSARBPROC glXCreateContextAttribsARB = NULL;
 
 /** The name the program was run with */
 static const char *program_name;
@@ -186,38 +189,227 @@ static void print_usage (void)
             "\nReport bugs to: <" PACKAGE_BUGREPORT ">\n", program_name);
 }
 
-/** Check GLX version and available extensions
- * @param display The display where application runs
- * @param screen the screen number where application runs
- * @returns 1 if minumum requred GLX version is available, 0 otherwise
+/** Frees universal OpenGL interface object
+ * @param ugl pointer to the object
  */
-static int initialize_glx (Display *display, int screen)
+static void ugl_free (UGL *ugl)
 {
-    const char *extensions = NULL;
+    free (ugl);
+}
 
-    /* Check the glx version */
-    if (glXQueryVersion (display, &glx_major, &glx_minor) == False) {
-        fprintf (stderr, "%s: glXQueryVersion fails\n", program_name);
-        return 0;
+/** Frees universal framebuffer configuration
+ * @param ugl pointer to UGL interface
+ * @param config Configuration to be freed
+ */
+static void ugl_free_framebuffer_config (const UGL *ugl,
+        UGLFrameBufferConfig *config)
+{
+    if (ugl->is_legacy) {
+        XFree (config);
     }
-    if ((glx_major < 1) || ((glx_major == 1) && (glx_minor < 2))) {
-        fprintf (stderr, "%s: atleast GLX 1.2 is required\n", program_name);
-        return 0;
-    } else if ((glx_minor == 2) && (glx_major == 1)) {
-        glx_legacy = 1;
+}
+
+/** Frees rendering surface
+ * @param ugl pointer to universal OpenGL interface object
+ * @param surface pointer to render surface object
+ */
+static void ugl_free_render_surface (const UGL *ugl, UGLRenderSurface *surface)
+{
+    glXDestroyContext (ugl->display, surface->context);
+    if (!ugl->is_legacy) {
+        glXDestroyWindow (ugl->display, surface->drawable);
     }
+    free (surface);
+}
 
-    /* Check available GLX extensions */
-    extensions = glXQueryExtensionsString (display, screen);
+/** Swap front and back buffers on a surface
+ * @param ugl pointer to universal OpenGL interface object
+ * @param surface pointer to render surface object
+ */
+static void ugl_swap_buffers (const UGL *ugl, const UGLRenderSurface *surface)
+{
+    glXSwapBuffers (ugl->display, surface->drawable);
+}
 
-    if (is_extension_supported (extensions, "GLX_ARB_create_context") != 0) {
-        glXCreateContextAttribsARB = (PFNGLXCREATECONTEXTATTRIBSARBPROC)
-                                     glXGetProcAddressARB ((const GLubyte *)"glXCreateContextAttribsARB");
-        arb_create_context_extension = glXCreateContextAttribsARB != NULL;
-        arb_context_profile = is_extension_supported (extensions,
-                              "GLX_ARB_create_context_profile");
+/** Make render surface to be current for OpenGL API
+ * @param ugl pointer to universal OpenGL interface object
+ * @param surface pointer to render surface object
+ * @returns non-zero on success, 0 otherwise
+ */
+static int ugl_make_current (const UGL *ugl, const UGLRenderSurface *surface)
+{
+    GLXDrawable drawable = (GLXDrawable)NULL;
+    GLXContext context = NULL;
+    if (surface != NULL) {
+        drawable = surface->drawable;
+        context = surface->context;
+    }
+    /* Make context current */
+    if (!ugl->is_legacy) {
+        glXMakeContextCurrent (ugl->display, drawable, drawable, context);
+    } else {
+        glXMakeCurrent (ugl->display, drawable, context);
     }
     return 1;
+}
+
+/** Create render surface assosiated with window
+ * @param ugl pointer to universal OpenGL interface object
+ * @param config pointer to framebuffer configuration object
+ * @param window native window handle
+ * @returns new render surface object, NULL otherwise
+ */
+static UGLRenderSurface *ugl_create_window_render_surface (const UGL *ugl,
+        UGLFrameBufferConfig *config, UGLNativeWindow window)
+{
+    GLXDrawable drawable = (GLXDrawable)NULL;
+    GLXContext context = NULL;
+    UGLRenderSurface *surface = NULL;
+    /* Creating openGL context */
+    if (!ugl->is_legacy) {
+        GLXFBConfig glx_config = (GLXFBConfig)config;
+        drawable = glXCreateWindow (ugl->display, glx_config, window, NULL);
+        if (ugl->glXCreateContextAttribsARB) {
+            int context_attribs[] = {
+                GLX_CONTEXT_MAJOR_VERSION_ARB, 1,
+                GLX_CONTEXT_MINOR_VERSION_ARB, 0,
+                None
+            };
+            context = ugl->glXCreateContextAttribsARB (ugl->display, glx_config,
+                      NULL, True, context_attribs);
+        } else {
+            context = glXCreateNewContext (ugl->display, glx_config,
+                                           GLX_RGBA_TYPE, NULL, True);
+        }
+        if (context == NULL) {
+            glXDestroyWindow (ugl->display, drawable);
+        }
+    } else {
+        context = glXCreateContext (ugl->display, (XVisualInfo *) config,
+                                    NULL, True);
+        drawable = (GLXDrawable) window;
+    }
+    if (context != NULL) {
+        surface = (UGLRenderSurface *) malloc (sizeof (UGLRenderSurface));
+        if (surface != NULL) {
+            surface->context = context;
+            surface->drawable = drawable;
+        }
+    }
+    return surface;
+}
+
+/** Get framebuffer configuration's attribute
+ * @param ugl pointer to universal OpenGL interface object
+ * @param config pointer to framebuffer configuration object
+ * @param attribute UGL attribute
+ * @param value pointer to value storage
+ * @returns non-zero if attribute retreived succesfully, 0 otherwise
+ */
+static int ugl_get_config_attribute (const UGL *ugl,
+                                     UGLFrameBufferConfig *config,
+                                     enum UGLConfigAttribute attribute,
+                                     void *value)
+{
+    if (attribute == UGL_NATIVE_VISUAL_ID) {
+        XVisualInfo *info = NULL;
+        VisualID *id = (VisualID *)value;
+        if (!ugl->is_legacy) {
+            info = glXGetVisualFromFBConfig (ugl->display, (GLXFBConfig)config);
+            if (info != NULL) {
+                *id = info->visualid;
+                XFree (info);
+            } else {
+                return 0;
+            }
+        } else {
+            info = (XVisualInfo *)config;
+            *id = info->visualid;
+        }
+        return 1;
+    }
+    return 0;
+}
+
+/** Get framebuffer configuration with specific attributes
+ * @param ugl pointer to universal OpenGL interface object
+ * @param attributes the array of GLX 1.3 attributes
+ * @returns framebuffer configuration object
+ */
+static UGLFrameBufferConfig *ugl_get_framebuffer_config (const UGL *ugl,
+        const int *attributes)
+{
+    UGLFrameBufferConfig *config = NULL;
+    GLXFBConfig *fbc = NULL;
+    int fbcount = 0;
+    if (!ugl->is_legacy) {
+        fbc = glXChooseFBConfig (ugl->display, ugl->screen, attributes,
+                                 &fbcount);
+        if (fbc != NULL) {
+            config = (UGLFrameBufferConfig *) fbc[0];
+            XFree (fbc);
+        }
+    } else {
+        /**TODO: convert attribs to GLX1.2 attribs list */
+        static int legacy_attribs[] = {
+            GLX_USE_GL, True,
+            GLX_RGBA, True,
+            GLX_RED_SIZE, 8,
+            GLX_GREEN_SIZE, 8,
+            GLX_BLUE_SIZE, 8,
+            GLX_ALPHA_SIZE, 8,
+            GLX_DEPTH_SIZE, 24,
+            GLX_STENCIL_SIZE, 8,
+            GLX_DOUBLEBUFFER, True,
+            None
+        };
+        config = (UGLFrameBufferConfig *)
+                 glXChooseVisual (ugl->display, ugl->screen, legacy_attribs);
+    }
+    return config;
+}
+
+/** Creates universal interface to OpenGL subsystem
+ * @param display The display where application runs
+ * @param screen the screen number where application runs
+ * @returns interface object, NULL otherwise
+ */
+static UGL *ugl_create (Display *display, int screen)
+{
+    UGL *ugl;
+    const char *extensions;
+    int major_version;
+    int minor_version;
+    /* Check the glx version */
+    if (glXQueryVersion (display, &major_version, &minor_version) == False) {
+        return NULL;
+    }
+    if ((major_version < 1) || ((major_version == 1) && (minor_version < 2))) {
+        return NULL;
+    }
+
+    ugl = (UGL *) malloc (sizeof (UGL));
+    if (ugl != NULL) {
+        ugl->display = display;
+        ugl->screen = screen;
+        ugl->glx_major = major_version;
+        ugl->glx_minor = minor_version;
+        ugl->is_legacy = ((minor_version == 2) && (major_version == 1));
+        /* Check available GLX extensions */
+        extensions = glXQueryExtensionsString (display, screen);
+
+        if (is_extension_supported (extensions, "GLX_ARB_create_context") != 0) {
+            ugl->glXCreateContextAttribsARB = (PFNGLXCREATECONTEXTATTRIBSARBPROC)
+                                              glXGetProcAddressARB ((const GLubyte *)"glXCreateContextAttribsARB");
+            ugl->is_arb_context_profile = is_extension_supported (extensions,
+                                          "GLX_ARB_create_context_profile");
+        } else {
+            ugl->glXCreateContextAttribsARB = NULL;
+            ugl->is_arb_context_profile = 0;
+        }
+    }
+
+    return ugl;
 }
 
 /** Parse command-line arguments
@@ -258,8 +450,14 @@ int main (int argc, char *const *argv)
         GLX_DOUBLEBUFFER, True,
         None
     };
-    int fbcount;
     int screen = 0;
+    UGL *ugl = NULL;
+    UGLFrameBufferConfig *ugl_config = NULL;
+    UGLRenderSurface *surface = NULL;
+    VisualID visual_id;
+    XVisualInfo *visual_info = NULL;
+    XVisualInfo visual_info_template;
+    int num_visuals;
     Display *display = NULL;
 
     parse_args (argc, argv);
@@ -272,110 +470,54 @@ int main (int argc, char *const *argv)
 
     screen = DefaultScreen (display);
 
-    if (!initialize_glx (display, screen)) {
+    if ((ugl = ugl_create (display, screen)) == NULL) {
         XCloseDisplay (display);
         return EXIT_FAILURE;
     }
-
-    /* Getting visual info to create window */
-    if (!glx_legacy) {
-        GLXFBConfig *fbc = glXChooseFBConfig (display, screen, attribs,
-                                              &fbcount);
-        if (fbc == NULL) {
-            fprintf (stderr, "%s: can't retrieve a framebuffer config\n",
-                     program_name);
-            return EXIT_FAILURE;
-        }
-        context_info.best_fbconfig = fbc[0];
-        XFree (fbc);
-        context_info.visual_info = glXGetVisualFromFBConfig (display,
-                                   context_info.best_fbconfig);
-    } else {
-        /**TODO: convert attribs to GLX1.2 attribs list */
-        static int legacy_attribs[] = {
-            GLX_USE_GL, True,
-            GLX_RGBA, True,
-            GLX_RED_SIZE, 8,
-            GLX_GREEN_SIZE, 8,
-            GLX_BLUE_SIZE, 8,
-            GLX_ALPHA_SIZE, 8,
-            GLX_DEPTH_SIZE, 24,
-            GLX_STENCIL_SIZE, 8,
-            GLX_DOUBLEBUFFER, True,
-            None
-        };
-        context_info.visual_info = glXChooseVisual (display, screen,
-                                   legacy_attribs);
+    if ((ugl_config = ugl_get_framebuffer_config (ugl, attribs)) == NULL) {
+        fprintf (stderr, "%s: can't retrieve a framebuffer config\n",
+                 program_name);
+        ugl_free (ugl);
+        return EXIT_FAILURE;
     }
-    if (context_info.visual_info == NULL) {
+    if (ugl_get_config_attribute (ugl, ugl_config, UGL_NATIVE_VISUAL_ID,
+                                  &visual_id)) {
+        memset (&visual_info_template, 0, sizeof (visual_info_template));
+        visual_info_template.visualid = visual_id;
+        visual_info = XGetVisualInfo (display, VisualIDMask,
+                                      &visual_info_template, &num_visuals);
+    }
+    if (visual_info == NULL) {
         fprintf (stderr, "%s: can't retrieve a visual\n", program_name);
+        ugl_free_framebuffer_config (ugl, ugl_config);
+        ugl_free (ugl);
         return EXIT_FAILURE;
     }
 
-    main_window = window_create (display, context_info.visual_info);
+    main_window = window_create (display, visual_info);
+    XFree (visual_info);
 
-    /* Creating openGL context */
-    if (!glx_legacy) {
-        context_info.glx_window = glXCreateWindow (display,
-                                  context_info.best_fbconfig,
-                                  main_window->xwindow, NULL);
-        if (arb_create_context_extension != 0) {
-            int context_attribs[] = {
-                GLX_CONTEXT_MAJOR_VERSION_ARB, 1,
-                GLX_CONTEXT_MINOR_VERSION_ARB, 0,
-                None
-            };
-            context_info.context = glXCreateContextAttribsARB (display,
-                                   context_info.best_fbconfig, NULL, True,
-                                   context_attribs);
-        } else {
-            context_info.context = glXCreateNewContext (display,
-                                   context_info.best_fbconfig, GLX_RGBA_TYPE,
-                                   NULL, True);
-        }
-    } else {
-        context_info.context = glXCreateContext (display,
-                               context_info.visual_info, NULL, True);
-    }
-    XFree (context_info.visual_info);
-    if (context_info.context == NULL) {
-        fprintf (stderr, "%s: can't create OpenGL context\n", program_name);
-        if (!glx_legacy) {
-            glXDestroyWindow (display, context_info.glx_window);
-        }
+    surface = ugl_create_window_render_surface (ugl, ugl_config,
+              main_window->xwindow);
+    if (surface == NULL) {
+        fprintf (stderr, "%s: can't create rendering surface\n", program_name);
         window_destroy (main_window);
+        ugl_free_framebuffer_config (ugl, ugl_config);
+        ugl_free (ugl);
         return EXIT_FAILURE;
     }
+    ugl_make_current (ugl, surface);
 
-    /* Make context current */
-    if (!glx_legacy) {
-        glXMakeContextCurrent (display, context_info.glx_window,
-                               context_info.glx_window, context_info.context);
-    } else {
-        glXMakeCurrent (display, main_window->xwindow, context_info.context);
-    }
-
-    /* Main loop */
     while (window_is_exists (main_window)) {
         window_process_events (main_window);
         /*game_tick();*/
-        if (!glx_legacy) {
-            glXSwapBuffers (display, context_info.glx_window);
-        } else {
-            glXSwapBuffers (display, main_window->xwindow);
-        }
+        ugl_swap_buffers (ugl, surface);
     }
-    /* Destroy and free all allocated data */
-    if (!glx_legacy) {
-        glXMakeContextCurrent (display, 0, 0, 0);
-    } else {
-        glXMakeCurrent (display, 0, 0);
-    }
-    glXDestroyContext (display, context_info.context);
-    if (!glx_legacy) {
-        glXDestroyWindow (display, context_info.glx_window);
-    }
+    ugl_make_current (ugl, NULL);
+    ugl_free_render_surface (ugl, surface);
     window_destroy (main_window);
+    ugl_free_framebuffer_config (ugl, ugl_config);
+    ugl_free (ugl);
     XCloseDisplay (display);
     return EXIT_SUCCESS;
 }
